3215b9aea394bad2b0547d32295bc37c46888611
Biswajit Biswas <bbiswajit1999sh@gmail.com>
2024-09-06 20:12:34 +0530

    Better error handling

--- src/middlewares/airplane-middleware.js
+++ src/middlewares/airplane-middleware.js
@@ -1,15 +1,13 @@
 const {StatusCodes} = require('http-status-codes');
+const {ErrorResponse} = require('../utils/common');
 
 function validateCreateRequest(req,res,next){
     if(!req.body.modelNumber){
+        ErrorResponse.message = "Something went wrong while creating airplane";
+        ErrorResponse.error = {explanation : `Model Number not found in the oncoming request in the correct form`}
         return res
         .status(StatusCodes.BAD_REQUEST)
-        .json({
-            success : false,
-            message : `Something went wrong while creating airplane`,
-            data : {},
-            error : {explanation : `Model Number not found`},
-        });
+        .json(ErrorResponse);
         
     }
     next();
