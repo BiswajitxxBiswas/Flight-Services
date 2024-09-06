6abbc642b0d10f59ee7e85988118fbb4bb0b9d72
Biswajit Biswas <bbiswajit1999sh@gmail.com>
2024-09-06 20:13:37 +0530

    Better Response and error handling

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -1,31 +1,25 @@
 const {AirplaneService} = require('../services');
 const {StatusCodes} = require('http-status-codes');
+const {SuccessResponse,ErrorResponse} = require('../utils/common');
 
 
 async function createAirplane(req,res) {
     try {
-        // console.log(req.body);
         const airplane = await AirplaneService.createAirplane({
             modelNumber : req.body.modelNumber,
             capacity : req.body.capacity
         });
+        SuccessResponse.message = "Successfully created an Airplane";
+        SuccessResponse.data = airplane;
         return res
             .status(StatusCodes.CREATED)
-            .json({
-                success : "true",
-                message : "Successfully created an Airplane",
-                data : airplane,
-                error:{}
-            });
+            .json(SuccessResponse);
     } catch (error) {
+        ErrorResponse.message = "Something went wrong while creating an Airplane";
+        ErrorResponse.error = error;
         return res
             .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({
-                success : "false",
-                message : "SOmething went wrong while creating an Airplane",
-                data : {},
-                error:error
-            });
+            .json(error);
     }
 }
 
