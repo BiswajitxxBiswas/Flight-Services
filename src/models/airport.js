9c53cbe2ff3c15d6f82459b32471f67d414a7d40
Biswajit Biswas <bbiswajit1999sh@gmail.com>
2024-10-13 13:06:17 +0530

    Added flight model with foreign key contraints during model definition ``` npx sequelize model:create --name Flight --attributes flightNumber:string,airplaneId:integer,departureAirportId:string,arrivalAirportId:string,arrivalTime:date,departureTime:date,price:integer,boardingGate:string,totalSeats:integer ```

--- src/models/airport.js
+++ src/models/airport.js
@@ -13,9 +13,15 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       this.belongsTo(models.City,{
         foreignKey : 'cityId',
-        onDelete : 'CASCADE',
-        onUpdate : 'CASCADE'
-      })
+      });
+      this.hasMany(models.Flight,{
+        foreignKey : 'departureAirportId',
+        onDelete : 'CASCADE'
+      });
+      this.hasMany(models.Flight,{
+        foreignKey : 'arrivalAirportId',
+        onDelete : 'CASCADE'
+      });
     }
   }
   Airport.init({
